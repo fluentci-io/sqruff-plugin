bfaff92142997a29e57b1e11c6673c4a4c92ca70
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-26 05:28:37 +0000

    ci: publish wasm to ghcr
    ci: publish wasm to ghcr
    
    ci: publish wasm to ghcr

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,6 +2,9 @@ use anyhow::Error;
 use fluentci_pdk::dag;
 
 pub fn setup_sqruff() -> Result<String, Error> {
+    let gh_token = dag().get_env("GITHUB_ACCESS_TOKEN")?;
+    dag().set_envs(vec![("GITHUB_ACCESS_TOKEN".into(), gh_token)])?;
+
     let stdout = dag()
         .pkgx()?
         .with_exec(vec![&format!(
